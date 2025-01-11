0d11ae46570412fc0d0f3f2fab66200d2a576409
mardoqueudickson1 <mardoqueudrums1999@gmail.com>
2025-01-11 12:00:26 +0100

    feat: fisrt screen

---
+++ components/CustumButtom.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustumButtom = () => {
+  return (
+    <View>
+      <Text>CustumButtom</Text>
+    </View>
+  )
+}
+
+export default CustumButtom
